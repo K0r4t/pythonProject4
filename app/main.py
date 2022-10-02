a108ce5c0e68defaafe482f65a86be6a13dc348d
K0r4t <pavlish.maxym@gmail.com>
2022-10-02 20:55:18 +0300

    update

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask('name')
+
+@app.route('/api/v1/hello-world-17')
+def hello_world():
+    return "Hello World 17"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+serve(app)
